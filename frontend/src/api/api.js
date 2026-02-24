2bbb6f96a549c05198959a0fdec0f4c2cb76d01e
atulacleaver <atulcleaver@gmail.com>
2026-02-24 08:01:21 +0530

    Reapply "feat: Update Product Card"
    This reverts commit 765eac28d91592f5e343871edb85994e88f64431.

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,19 @@
-import axios from "axios";
+import axios from 'axios'
 
-export const api = axios.create({
-  baseURL: "http://localhost:8000",
-  timeout: 10000,
-});
+const API_BASE_URL = 'http://localhost:8000/'
+
+const api = axios.create({
+    baseURL: API_BASE_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+})
+
+export const getRecommendations = async (formData) => {
+    const response = await api.post('/recommend', {
+        budget: Number(formData.budget),
+        health_condition: formData.healthCondition || null,
+        household_size: Number(formData.householdSize),
+    })
+    return response.data
+}
--- frontend/src/api/recommend.js
+++
@@ -1,5 +0,0 @@
-import { api } from "./client";
-
-export const getRecommendations = async (payload) => {
-  return (await api.post("/recommend", payload)).data;
-};
